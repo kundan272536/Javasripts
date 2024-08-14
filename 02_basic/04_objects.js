be83d80ca732711cba32d327b3e7ba601c523174
KUNDAN KUMAR <103889218+kundan272536@users.noreply.github.com>
2024-08-14 22:06:22 +0530

    object

---
+++ 02_basic/04_objects.js
@@ -0,0 +1,8 @@
+const tinderUser=new Object(); //Singleton ojects
+
+tinderUser.id="123abc";
+tinderUser.name="Kundan";
+tinderUser.email="kundankumar@gmail.com";
+tinderUser.isLoggedIn=false;
+console.log(tinderUser);
+
