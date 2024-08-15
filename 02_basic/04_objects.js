67f7df86cdad66d1161ec01128f93712559cad3d
KUNDAN KUMAR <103889218+kundan272536@users.noreply.github.com>
2024-08-15 10:30:30 +0530

    objects data types done

--- 02_basic/04_objects.js
+++ 02_basic/04_objects.js
@@ -4,5 +4,47 @@ tinderUser.id="123abc";
 tinderUser.name="Kundan";
 tinderUser.email="kundankumar@gmail.com";
 tinderUser.isLoggedIn=false;
-console.log(tinderUser);
+// console.log(tinderUser);
+
+const regularUser={
+    email:"somea@gmail.com",
+    fullname:{
+        userfullname:{
+            firstname:"kundan",
+            lastname:"kumar",
+        }
+    }
+}
+// console.log(regularUser.fullname.userfullname.firstname);
+
+const obj1={1:"a",2:"b"};
+const obj2={3:"c",4:"d"};
+const obj3=Object.assign({},obj1,obj2);
+const obj4={...obj1,...obj2}
+console.log(obj4);
+
+const user=[
+    {
+        name:"kundan",
+        age:"24"
+    },
+    {
+        email:"kundankumar252736@gmail.com",
+    }
+]
+
+console.log(user[1].email);
+
+// console.log(tinderUser);
+// console.log(Object.keys(tinderUser));
+// console.log(Object.values(tinderUser));
+// console.log(tinderUser.hasOwnProperty('isLoggedIn'));
+
+const course={
+    courseName:"kundan",
+    courseInstructor:"kundan kumar",
+    coursePrice:"999"
+}
+const {courseName:name,courseInstructor:Instructor}=course;
+console.log(name,Instructor);
 
