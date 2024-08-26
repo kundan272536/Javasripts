e264913b2650b71cd0d6132b0dac5e9ca05ea6a3
KUNDAN KUMAR <103889218+kundan272536@users.noreply.github.com>
2024-08-26 22:01:26 +0530

    project_one_completed

---
+++ 07_Projects/01_Project/index.js
@@ -0,0 +1,23 @@
+const button=document.querySelectorAll('.button');
+console.log(button);
+const body=document.querySelector('body');
+console.log(body);
+
+button.forEach(function(button){
+    console.log(button);
+    button.addEventListener('click',function(e){
+        console.log(e);
+        console.log(e.target.id);
+        if(e.target.id==='grey'){
+            body.style.background=e.target.id
+        }
+        if(e.target.id==='white'){
+            body.style.background=e.target.id
+        }
+        if(e.target.id==='blue'){
+            body.style.background=e.target.id
+        } if(e.target.id==='yellow'){
+            body.style.background=e.target.id
+        }
+    });
+});
